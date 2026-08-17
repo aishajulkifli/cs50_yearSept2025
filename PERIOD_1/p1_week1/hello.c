ec05c9de4bb54fe848b4d9291916ecaa468c5589
Aisha Julkifli <aisha.aisha.binti.julkifli@student.nhlstenden.com>
2026-08-17 22:09:46 +0000

    Tue, Aug 18, 2026, 12:09 AM +02:00

--- PERIOD_1/p1_week1/hello.c
+++
@@ -1,9 +0,0 @@
-#include <cs50.h>       // get_string function
-#include <stdio.h>      // printf function
-
-int main(void)
-{
-    string firstname = get_string("What's your name? ");    // prompt for user name
-    printf("hello, %s\n", firstname);                       // greet the user
-}
-
