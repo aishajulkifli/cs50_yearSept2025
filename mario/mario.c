e307c82635f157c270b90bf8127d795ef6a25222
Aisha Julkifli <aisha.aisha.binti.julkifli@student.nhlstenden.com>
2026-08-20 00:26:51 +0000

    Thu, Aug 20, 2026, 2:26 AM +02:00

--- mario/mario.c
+++ mario/mario.c
@@ -3,6 +3,25 @@
 
 int main(void)
 {
-    int height = get_int("Height: ");
-    printf("#\n", height);
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < 1 || height > 8);
+
+    for (int i = 0; i < height; i++)
+    {
+        print_row(i + 1);
+    }
+
+}
+
+void print_row(int bricks)
+{
+    for (int i = 0; i < bricks; i++)
+    {
+        printf("#");
+    }
+    printf("\n");
 }
