c199a04a4689057200c85575a0f5706d01e7aa6d
Aisha Julkifli <aisha.aisha.binti.julkifli@student.nhlstenden.com>
2026-08-20 08:13:13 +0000

    Thu, Aug 20, 2026, 10:13 AM +02:00

---
+++ mario-more/mario.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <cs50.h>
+
+void print_row(int spaces, int bricks);
+
+int main(void)
+{
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < 1 || height > 8);
+
+    for (int i = 0; i < height; i++)
+    {
+        print_row(height - i - 1, i + 1);
+    }
+
+}
+
+void print_row(int spaces, int bricks)
+{
+    for (int i = 0; i < spaces; i++)
+    {
+        printf(" ");
+    }
+
+    for (int i = 0; i < bricks; i++)
+    {
+        printf("#");
+    }
+    printf("\n");
+}
