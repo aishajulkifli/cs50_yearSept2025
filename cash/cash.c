2b9c67d142b18a2217e2d4fa76c60ab41b7001d1
Aisha Julkifli <aisha.aisha.binti.julkifli@student.nhlstenden.com>
2026-08-21 14:21:06 +0000

    Fri, Aug 21, 2026, 4:21 PM +02:00

--- cash/cash.c
+++ cash/cash.c
@@ -1,7 +1,10 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int calculate_quarters(int cents);(int cents);
+int calculate_quarters(int cents);
+int calculate_dimes(int cents);
+int calculate_nickels(int cents);
+int calculate_pennies(int cents);
 
 int main(void)
 {
@@ -19,3 +22,17 @@ int calculate_quarters(int cents)
     return cents / 25;
 }
 
+int calculate_dimes(int cents)
+{
+    return cents / 10;
+}
+
+int calculate_nickels(int cents)
+{
+    retun cents / 5;
+}
+
+int calculate_pennies(int cents)
+{
+    return cents / 1;
+}
